a828ba2278df45544309e89be9412014501dbf03
alirezafeizabadi55 <alirezafeizabadi55@gmail.com>
2026-07-10 17:52:45 +0330

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function toPersianNumber(num: number | string): string {
+  const persianDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+  return num.toString().replace(/\d/g, (d) => persianDigits[parseInt(d)]);
+}
+
+export function formatPrice(price: number): string {
+  return toPersianNumber(price.toLocaleString('fa-IR'));
+}
